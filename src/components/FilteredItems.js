4c2aa00c90a958a08e2e5b0cc32db76c431fa185
jnicolon <63628726+jnicolon@users.noreply.github.com>
2020-07-28 15:36:25 -0400

    component FilteredItems

--- src/components/FilteredItems.js
+++ src/components/FilteredItems.js
@@ -1,7 +1,40 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import buyItem from "../redux/actions/goldActions";
 
-export default class FilteredItems extends Component {
+class FilteredItems extends Component {
   render() {
-    return <div></div>;
+    const filteredItems = this.props.items.items.filter(
+      (key) => `/${key.fields.type}` === this.props.path
+    );
+
+    const renderItems = filteredItems.map((item) => {
+      return (
+        <div className="single-item" key={item.fields.id}>
+          <img src={item.fields.image.fields.file.url} alt={item.fields.id} />
+          <p>{item.fields.itemName}</p>
+          <p>{item.fields.price} gold</p>
+          <button onClick={() => this.props.buyItem(item.fields.price)}>
+            Buy
+          </button>
+        </div>
+      );
+    });
+
+    return <div className="item-container">{renderItems}</div>;
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    items: state.items,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    buyItem: (price) => dispatch(buyItem(price)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilteredItems);
--- src/pages/Armor.js
+++ src/pages/Armor.js
@@ -1,30 +1,15 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import filteredItems from "../functionality/filteredItems";
-import buyItem from "../redux/actions/goldActions";
+import FilteredItems from "../components/FilteredItems";
 
 class Armor extends Component {
   render() {
-    const renderItems = filteredItems(
-      this.props.location.pathname,
-      this.props.items.items,
-      this.props.buyItem
+    const path = this.props.location.pathname;
+    return (
+      <>
+        <FilteredItems path={path} />
+      </>
     );
-
-    return <div className="item-container">{renderItems}</div>;
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    items: state.items,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    buyItem: (price) => dispatch(buyItem(price)),
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Armor);
+export default Armor;
