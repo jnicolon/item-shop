626afbe37be7f6f1e7fb5ffb6b2371b57cfef6c3
jnicolon <63628726+jnicolon@users.noreply.github.com>
2020-08-08 19:39:14 -0400

    intro change with state display

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -2,21 +2,27 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class Intro extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true,
+    };
+  }
+
   render() {
     return (
-      <div
-        className={
-          window.location.pathname === "/"
-            ? "intro dispaly-on"
-            : "intro display-off"
-        }
-      >
+      <div className={this.state.display ? "intro" : "intro display-off"}>
         <h4>
           I guess in previous times they would have refered to you as
           WeaponMaster but today you will be known as...
         </h4>
         <h1>MEDIEVAL PERSONAL SHOPPER</h1>
-        <Link to="/levelIntro">Start adventure!</Link>
+        <Link
+          onClick={() => this.setState({ display: false })}
+          to="/levelIntro"
+        >
+          Start adventure!
+        </Link>
       </div>
     );
   }
