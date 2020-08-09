42c3a755b51c193da7cef452947aaaf123bcd50d
jnicolon <63628726+jnicolon@users.noreply.github.com>
2020-08-08 20:16:15 -0400

    hashbrowser

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -4,13 +4,7 @@ import { Link } from "react-router-dom";
 export default class Intro extends Component {
   render() {
     return (
-      <div
-        className={
-          window.location.pathname === "/"
-            ? "intro dispaly-on"
-            : "intro display-off"
-        }
-      >
+      <div className="intro">
         <h4>
           I guess in previous times they would have refered to you as
           WeaponMaster but today you will be known as...
