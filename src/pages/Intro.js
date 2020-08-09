81c021333ca884b6a117f7ab9269887b8441d653
jnicolon <63628726+jnicolon@users.noreply.github.com>
2020-08-08 20:07:37 -0400

    Revert "intro change with state display"
    This reverts commit 626afbe37be7f6f1e7fb5ffb6b2371b57cfef6c3.

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -2,27 +2,21 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class Intro extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      display: true,
-    };
-  }
-
   render() {
     return (
-      <div className={this.state.display ? "intro" : "intro display-off"}>
+      <div
+        className={
+          window.location.pathname === "/"
+            ? "intro dispaly-on"
+            : "intro display-off"
+        }
+      >
         <h4>
           I guess in previous times they would have refered to you as
           WeaponMaster but today you will be known as...
         </h4>
         <h1>MEDIEVAL PERSONAL SHOPPER</h1>
-        <Link
-          onClick={() => this.setState({ display: false })}
-          to="/levelIntro"
-        >
-          Start adventure!
-        </Link>
+        <Link to="/levelIntro">Start adventure!</Link>
       </div>
     );
   }
