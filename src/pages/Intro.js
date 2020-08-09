500639a60b153d6444323ab57e5f4a899da79807
jnicolon <63628726+jnicolon@users.noreply.github.com>
2020-08-08 20:07:02 -0400

    Update Intro.js

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -2,28 +2,35 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export default class Intro extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      display: true,
-    };
-  }
-
   render() {
     return (
+<<<<<<< HEAD
       // <div className={this.state.display ? "intro" : "intro display-off"}>
       <div className="intro">
+=======
+      <div
+        className={
+          window.location.pathname === "/"
+            ? "intro dispaly-on"
+            : "intro display-off"
+        }
+      >
+>>>>>>> parent of 626afbe... intro change with state display
         <h4>
           I guess in previous times they would have refered to you as
           WeaponMaster but today you will be known as...
         </h4>
         <h1>MEDIEVAL PERSONAL SHOPPER</h1>
+<<<<<<< HEAD
         <Link
           //onClick={() => this.setState({ display: false })}
           to="/levelIntro"
         >
           Start adventure!
         </Link>
+=======
+        <Link to="/levelIntro">Start adventure!</Link>
+>>>>>>> parent of 626afbe... intro change with state display
       </div>
     );
   }
