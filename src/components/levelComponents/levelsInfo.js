660a199e83733f11f1b0a39de35acb135245b5af
jnicolon <63628726+jnicolon@users.noreply.github.com>
2020-08-12 16:47:23 -0400

    level 3 added

--- src/components/levelComponents/levelsInfo.js
+++ src/components/levelComponents/levelsInfo.js
@@ -1,23 +1,26 @@
 import chillBradImg from "../../imgs/chillBrad.png";
 import { level1Intro } from "../textComponents/level1Dialogue";
 import { level2Intro } from "../textComponents/level2Dialogue";
+import { level3Intro } from "../textComponents/level3Dialogue";
 import enemy1Img from "../../imgs/unnamed.png";
 import enemy2Img from "../../imgs/yoga.jpg";
-import hero2img from "../../imgs/drunk.png";
+import enemy3Img from "../../imgs/bigBoy.jpg";
+import hero3Img from "../../imgs/mindFreak.jpg";
+import hero2Img from "../../imgs/drunk.png";
 
 export const levels = [
   {
     level: 1,
     hero: {
       name: "Chill Brad",
-      gold: 1000,
+      gold: 12,
       img: chillBradImg,
     },
     enemy: {
       name: "A Tiny Orc",
       stats: {
-        hp: 15,
-        atk: 3,
+        hp: 25,
+        atk: 45,
         mana: 0,
       },
       img: enemy1Img,
@@ -29,7 +32,7 @@ export const levels = [
     hero: {
       name: "Mongor",
       gold: 36,
-      img: hero2img,
+      img: hero2Img,
     },
     enemy: {
       name: "Limber Johnny",
@@ -41,5 +44,23 @@ export const levels = [
       img: enemy2Img,
     },
     introDialogue: level2Intro,
+  },
+  {
+    level: 3,
+    hero: {
+      name: "MindFreak",
+      gold: 75,
+      img: hero3Img,
+    },
+    enemy: {
+      name: "Big Boy Welder",
+      stats: {
+        hp: 250,
+        atk: 5,
+        mana: 0,
+      },
+      img: enemy3Img,
+    },
+    introDialogue: level3Intro,
   },
 ];
---
+++ src/components/textComponents/level3Dialogue.js
@@ -0,0 +1,35 @@
+import hero3 from "../../imgs/mindFreak.jpg";
+import tavernKeeper from "../../imgs/tavern.png";
+
+export const level3Intro = [
+  {
+    character: "tavernKeeper",
+    image: tavernKeeper,
+    dialogue: "You have a good eye for shopping!",
+  },
+  {
+    character: "tavernKeeper",
+    image: tavernKeeper,
+    dialogue: "Keep at it!",
+  },
+  {
+    character: "tavernKeeper",
+    image: tavernKeeper,
+    dialogue: "Oh, no. I hate the next guy...",
+  },
+  {
+    character: "hero3",
+    image: hero3,
+    dialogue: "MINDFREAK, BABY!",
+  },
+  {
+    character: "hero3",
+    image: hero3,
+    dialogue: "MINDFREAK, BABY!",
+  },
+  {
+    character: "hero3",
+    image: hero3,
+    dialogue: "LET'S GO!",
+  },
+];
